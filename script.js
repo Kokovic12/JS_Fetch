7fb9eccb72d8761a8f324e4b01690df7fc2bde9d
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-10-25 21:33:20 +0300

    feat:add file

---
+++ script.js
@@ -0,0 +1,102 @@
+const API_TOKEN = 'YmVmYjliYmQtN2U0MC00MzkyLTgwNjUtYTA1ZTRkMzAwZTc0';
+const API_URL = 'https://api.m3o.com/v1/weather/Forecast';
+
+fetch(API_URL +  `?days=${days}&location=${location}` , {
+        method:"GET",
+        headers:{
+            "Content-Type": `application/json`,
+            "Authorization": `Bearer ${API_TOKEN}`,
+        }
+     })
+     .then(resp=>resp.json())     
+     .then((data)=>{output.innerHTML = JSON.stringify( data, null, '\t')})
+     .catch((error)=>{output.innerHTML = error})
+
+const inputDays = document.querySelector('.days');           
+const inputCity = document.querySelector('.city');                
+const button_click = document.querySelector('.click_button');                  
+const output = document.querySelector('.output');         
+
+      inputDays.addEventListener('change', () => {
+        const resultDays = document.querySelector('.resultDays');
+      });
+
+     inputCity.addEventListener('change', () => {
+        const resultCity = document.querySelector('.resultCity');
+     });
+
+    //  button_click.addEventListener('click', () => {
+
+
+
+
+
+
+//   Бредовый БРЕД
+    //  function render(data){
+    //      renderCity(data);
+    //      renderDays(data);
+
+    //  }
+
+    //  function renderCity(data){
+    //      let cityName=document.querySelector('.current_city');
+    //      cityName.innerHTML=data.city.name;
+    //  }
+
+    //  function renderDays(data){
+    //     let cityDays=document.querySelector('.current_days');
+    //     cityDays.innerHTML=data.days.name;
+    // }
+
+
+
+// Example 1
+// const API_TOKEN = 'YmVmYjliYmQtN2U0MC00MzkyLTgwNjUtYTA1ZTRkMzAwZTc0';
+// const API_URL = 'https://api.m3o.com/v1/weather/Forecast';
+// const API_Params = new URLSearchParams({
+//     foo: 'value',
+//     bar: 2,
+// })
+
+
+// fetch(API_URL + API_Params,  {
+//         method:"GET",
+//         headers:{
+//             "Content-Type": `application/json`,
+//             "Authorization": `Bearer ${API_TOKEN}`,
+//         }
+//      })
+//      .then(resp=>resp.json())
+//      .then((data)=>{console.log(data)})
+//      .catch((error)=>{console.log(error)})
+
+
+
+// async function getWeather(method, url){
+//     const resp = await fetch (url, {
+//         method: 'GET',
+//         headers: {
+//             "Content-Type": `application/json`,
+//             "Authorization": `Bearer ${API_TOKEN}`,   
+//         },
+//     })
+
+//     .then(respData=>resp.json())
+//     console.log(respData);
+// }
+
+// getWeather(API_URL);
+
+
+
+
+// fetch('https://example.com'? + new URLSearchParams({
+//     foo: 'value',
+//     bar: 2,
+// }))
+
+
+//https://api.example.com?query1=value1&query2=value2
+
+
