8df5b76cc1a3a14201e0f9293c81dd7590efb1cf
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-10-27 08:37:57 +0300

    fix: changed format code

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-const API_TOKEN = 'YmVmYjliYmQtN2U0MC00MzkyLTgwNjUtYTA1ZTRkMzAwZTc0';
-const API_URL = 'https://api.m3o.com/v1/weather/Forecast';
+const API_TOKEN = 'YmVmYjliYmQtN2U0MC00MzkyLTgwNjUtYTA1ZTRkMzAwZTc0'
+const API_URL = 'https://api.m3o.com/v1/weather/Forecast'
 
 fetch(API_URL +  `?days=${days}&location=${location}` , {
-        method:"GET",
-        headers:{
-            "Content-Type": `application/json`,
-            "Authorization": `Bearer ${API_TOKEN}`,
-        }
-     })
-     .then(resp=>resp.json())     
-     .then((data)=>{output.textContent = JSON.stringify( data, null, '\t')})
-     .catch((error)=>{output.textContent = error})
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_TOKEN}`,
+    }}).then(resp => resp.json())     
+     .then((data) => {
+        output.textContent = JSON.stringify(data, null, '\t')
+    })
+     .catch((error) => {
+        output.textContent = error
+    })
 
-const inputDays = document.querySelector('.days');           
-const inputCity = document.querySelector('.city');                
-const button_click = document.querySelector('.click_button');                  
-const output = document.querySelector('.output');         
+const inputDays = document.querySelector('.days')           
+const inputCity = document.querySelector('.city')                
+const button_click = document.querySelector('.click_button')                  
+const output = document.querySelector('.output')         
 
       inputDays.addEventListener('change', () => {
         const resultDays = document.querySelector('.resultDays');
         resultDays.textContent= inputDays.value;
-      });
+      })
 
      inputCity.addEventListener('change', () => {
-        const resultCity = document.querySelector('.resultCity');
-        resultCity.textContent=inputCity.value;
-     });
+        const resultCity = document.querySelector('.resultCity')
+        resultCity.textContent = inputCity.value
+     })
 
     //  button_click.addEventListener('click', () => {
 
