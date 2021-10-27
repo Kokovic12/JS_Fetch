9f7903fae3d4cd7a786983c5d8a932d8d939f2a6
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-10-27 11:20:08 +0300

    fix: add click

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
 const API_TOKEN = 'YmVmYjliYmQtN2U0MC00MzkyLTgwNjUtYTA1ZTRkMzAwZTc0'
 const API_URL = 'https://api.m3o.com/v1/weather/Forecast'
 
-fetch(API_URL +  `?days=${days}&location=${location}` , {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_TOKEN}`,
-    }}).then(resp => resp.json())     
-     .then((data) => {
-        output.textContent = JSON.stringify(data, null, '\t')
-    })
-     .catch((error) => {
-        output.textContent = error
-    })
+function getFetch() {             
+        fetch(API_URL +  `?days=${days}&location=${location}` , {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_TOKEN}`,
+        }}).then(resp => resp.json())   
+         .then((data) => {
+            output.textContent = JSON.stringify(data, null, '\t')
+        })
+         .catch((error) => {
+            output.textContent = error
+        })
+    }
 
 const inputDays = document.querySelector('.days')           
-const inputCity = document.querySelector('.city')                
-const button_click = document.querySelector('.click_button')                  
+const inputCity = document.querySelector('.city')                  
 const output = document.querySelector('.output')         
 
-      inputDays.addEventListener('change', () => {
-        const resultDays = document.querySelector('.resultDays');
-        resultDays.textContent= inputDays.value;
-      })
 
-     inputCity.addEventListener('change', () => {
-        const resultCity = document.querySelector('.resultCity')
-        resultCity.textContent = inputCity.value
-     })
+    //   inputDays.addEventListener('change', () => {
+    //     const resultDays = document.querySelector('.resultDays');
+    //     resultDays.textContent= inputDays.value;
+    //   })
 
-    //  button_click.addEventListener('click', () => {
-
+    //  inputCity.addEventListener('change', () => {
+    //     const resultCity = document.querySelector('.resultCity')
+    //     resultCity.textContent = inputCity.value
+    //  })
 
 
-
+// var btn = document.querySelector('.btn');
+// btn.onclick = function () { 
+//     console.log(true);
+//     fetch(API_URL +  `?days=${days}&location=${location}` , {
+//     method: 'GET',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${API_TOKEN}`,
+//     }}).then(resp => resp.json())     
+//      .then((data) => {
+//         output.textContent = JSON.stringify(data, null, '\t')
+//     })
+//      .catch((error) => {
+//         output.textContent = error
+//     })}
 
 
 //   Бредовый БРЕД
